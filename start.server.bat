62f6bf30778460d6b67edf10a29d493beb0765a0
PshsayhiXD <maiphucthinhaltx@gmail.com>
2026-05-17 11:03:45 +0700

    Added esbuild and @types/localtunnel to devDependencies

---
+++ start.server.bat
@@ -0,0 +1,5 @@
+@echo off
+TITLE Dredbot Evolve - Server
+color 0A
+echo [!] Starting server...
+npm run start:server
---
+++ start.server.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo "Starting drednot client server..."
+npm run start:server
+echo "Done."
