bc13fff624b9b40700508f5f7b4cf7a998d41771
PshsayhiXD <maiphucthinhaltx@gmail.com>
2026-05-26 10:45:41 +0700

    Updated currency icons to use emojis

--- src/configs/currency.ts
+++ src/configs/currency.ts
@@ -1,4 +1,5 @@
 import { Decimal } from "decimal.js";
+import { EMOJIS } from "@configs/emojis.js";
 
 const BASE = new Decimal("1e-16");
 
@@ -7,19 +8,19 @@ export const CURRENCY = {
   COLUMN_NAME: "currency",
   BASE,
   units: {
-    "<:res_flux_crystals:1502673687160291450>": BASE.mul(1_000_000_000),
-    "<:res_hyper_rubber:1502673706709680148>": BASE.mul(10_000_000),
-    "<:res_silica_crystals:1502673754390790184>": BASE.mul(100_000),
-    "<:comp_iron:1502673596638564562>": BASE.mul(1000),
-    "<:comp_exp:1502673571594502295>": BASE.mul(100),
-    "<:res_metal:1502673731242299492>": BASE.mul(10),
-    "<:res_explosives:1502673665106514132>": BASE.mul(1),
+    [EMOJIS.res_flux_crystals]: BASE.mul(1_000_000_000),
+    [EMOJIS.res_hyper_rubber]: BASE.mul(10_000_000),
+    [EMOJIS.res_silica_crystals]: BASE.mul(100_000),
+    [EMOJIS.comp_metal]: BASE.mul(1000),
+    [EMOJIS.comp_exp]: BASE.mul(100),
+    [EMOJIS.res_metal]: BASE.mul(10),
+    [EMOJIS.res_explosives]: BASE.mul(1),
 
     "[res_flux_crystals]": BASE.mul(1_000_000_000),
     "[res_hyper_rubber]": BASE.mul(10_000_000),
     "[res_silica_crystals]": BASE.mul(100_000),
     "[comp_metal]": BASE.mul(1000),
-    "[comp_iron]": BASE.mul(1000),
+    "[comp_iron]": BASE.mul(1000), // aliases
     "[comp_exp]": BASE.mul(100),
     "[res_metal]": BASE.mul(10),
     "[res_explosives]": BASE.mul(1),
@@ -41,13 +42,13 @@ export const CURRENCY = {
   } as const,
 
   formatUnits: [
-    ["<:res_flux_crystals:1502673687160291450>", BASE.mul(1_000_000_000)],
-    ["<:res_hyper_rubber:1502673706709680148>", BASE.mul(10_000_000)],
-    ["<:res_silica_crystals:1502673754390790184>", BASE.mul(100_000)],
-    ["<:comp_iron:1502673596638564562>", BASE.mul(1000)],
-    ["<:comp_exp:1502673571594502295>", BASE.mul(100)],
-    ["<:res_metal:1502673731242299492>", BASE.mul(10)],
-    ["<:res_explosives:1502673665106514132>", BASE.mul(1)],
+    [EMOJIS.res_flux_crystals, BASE.mul(1_000_000_000)],
+    [EMOJIS.res_hyper_rubber, BASE.mul(10_000_000)],
+    [EMOJIS.res_silica_crystals, BASE.mul(100_000)],
+    [EMOJIS.comp_metal, BASE.mul(1000)],
+    [EMOJIS.comp_exp, BASE.mul(100)],
+    [EMOJIS.res_metal, BASE.mul(10)],
+    [EMOJIS.res_explosives, BASE.mul(1)],
   ] as const,
 
 } as const;
